acd84d3be7d79c2f6398dbb7626b91952fcbc5cf
LexMorrison <borrlex11@gmail.com>
2023-08-10 01:21:42 +0200

    fix positioning

--- src/components/Pages/MovieDetails.styled.js
+++ src/components/Pages/MovieDetails.styled.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   padding: 0 16px;
-  margin: 0 auto;
 `;
 
 export const Button = styled(Link)`
+  position: relative;
   display: inline-block;
   padding: 10px 20px;
   font-size: 16px;
@@ -16,7 +16,7 @@ export const Button = styled(Link)`
   border-radius: 5px;
   transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
   margin-bottom: 10px;
-
+  top: -25px;
   &:hover {
     background-color: #ddd;
     color: black;
@@ -25,6 +25,8 @@ export const Button = styled(Link)`
 
 export const PositionDiv = styled.div`
   display: flex;
+  position: relative;
+  top: -15px;
 `;
 
 export const TextPos = styled.div`
