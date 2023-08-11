de9820f7b5e79b428985812715c56465747397a0
LexMorrison <119893721+LexMorrison@users.noreply.github.com>
2023-08-11 10:25:29 +0000

    Deploying to gh-pages from @ LexMorrison/goit-react-hw-05-movies@2c37d1e22e2a63835d3a9de015cceb8b56c15f0d 🚀

---
+++ static/js/673.b2f018b8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[673],{2673:function(e,n,s){s.r(n);var i=s(2791),t=s(7689),r=s(184),u=(0,i.lazy)((function(){return Promise.all([s.e(816),s.e(381)]).then(s.bind(s,476))}));n.default=function(){return(0,r.jsxs)(i.Suspense,{children:[(0,r.jsx)(u,{}),(0,r.jsx)("main",{children:(0,r.jsx)(t.j3,{})})]})}}}]);
+//# sourceMappingURL=673.b2f018b8.chunk.js.map
